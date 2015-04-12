09b85cfbb37ef37446d34819c853fba98ea0b222
Michael J. Bennett <dev@michaeljbennett.info>
2015-04-12 17:43:40 -0400

    Updated coverage to integrate with CI/Code Climate, added coverage badge
     On branch master
    	modified:   .travis.yml
    	modified:   README.md
    	modified:   etc/karma.conf.js

--- etc/karma.conf.js
+++ etc/karma.conf.js
@@ -80,6 +80,19 @@ module.exports = function(config) {
 
         ngHtml2JsPreprocessor: {
             stripPrefix: 'src/'
+        },
+
+        coverageReporter: {
+            dir: 'coverage/',
+            reporters: [
+                { type: 'html', subdir: 'report-html' },
+                { type: 'lcov', subdir: 'report-lcov' },
+                { type: 'cobertura', subdir: '.', file: 'cobertura.txt' },
+                { type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' },
+                { type: 'teamcity', subdir: '.', file: 'teamcity.txt' },
+                { type: 'text', subdir: '.', file: 'text.txt' },
+                { type: 'text-summary', subdir: '.', file: 'text-summary.txt' }
+            ]
         }
     });
 };
